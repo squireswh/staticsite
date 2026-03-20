031dbcb3e7f756df8f67c1ad8f50b1cfa09b7c49
squireswh <wsquires@satx.rr.com>
2026-03-20 13:41:55 -0500

    Fix main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 # main.py
 #
 # (c) 2026 Boot.dev
+from textnode import TextType, TextNode
 
 def main():
-	pass
+	new_node = TextNode("This is some anchor text", TextType.TEXT_LINK, "https://www.boot.dev")
+	print(new_node)
 
-if __name__=="__main__":
+if __name__ == "__main__":
 	main()
 
